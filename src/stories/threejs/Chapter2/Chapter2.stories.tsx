2b8cb97821eee16c46b5abe11bf56cd9f7b39c51
bugtype <bugtype.kr@gmail.com>
2021-03-14 12:31:14 +0900

    feature: 안개 및 요약 정리 추가

--- src/stories/threejs/Chapter2/Chapter2.stories.tsx
+++ src/stories/threejs/Chapter2/Chapter2.stories.tsx
@@ -1,5 +1,6 @@
 import { Meta, Story } from '@storybook/react';
 import { Chapter2 } from './Chapter2';
+import { Chapter2_2 } from './Chapter2-2';
 
 export default {
   title: 'Threejs/Chapter2',
@@ -9,4 +10,10 @@ const Template1: Story = (args) => {
   return <Chapter2 {...args} />;
 };
 
+const Template2: Story = (args) => {
+  return <Chapter2_2 {...args} />;
+};
+
 export const scene1 = Template1.bind({});
+
+export const scene2 = Template2.bind({});
